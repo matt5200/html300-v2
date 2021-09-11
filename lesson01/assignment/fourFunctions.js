96c4711f6fb65dc6ae797b20f327942c2795bad6
matthew <leematt5@uw.edu>
2021-09-11 10:22:34 -0700

    added functions

--- lesson01/assignment/fourFunctions.js
+++ lesson01/assignment/fourFunctions.js
@@ -2,16 +2,16 @@
 document. addEventListener("DOMContentLoaded", function(){
 
   // Find the example form, and handle its submit event
-  const exampleForm = document.querySelector('#four-functions')
+  const exampleForm = document.getElementById("function-1")
 
   exampleForm.addEventListener('submit', function(event){
     // Prevent the form from trying to submit to a server
     event.preventDefault()
 
     // An array to be manipulated
-    let data = [1, 2, 3]
+    let data = [120, 270, 35]
 
-    const inputField = document.querySelector('#four-functions .input')
+    const inputField = document.querySelector('#function-1 .input')
     const userInput = inputField.value
     const userNumber = parseInt(userInput)
 
@@ -19,13 +19,45 @@ document. addEventListener("DOMContentLoaded", function(){
     const ret = data.map( x =>  x % userNumber);
 
     // TODO
-    document.querySelector('#four-functions .output').innerHTML = ret.toString();
+    document.querySelector('#function-1 .output').innerHTML = ret.toString();
 
     // Clear the form field so the user can try again
     inputField.value = ''
   })
+})
+
+document. addEventListener("DOMContentLoaded", function(){
+
+  // Find the example form, and handle its submit event
+  const exampleForm = document.getElementById('function-2')
+
+  exampleForm.addEventListener('submit', function(event){
+    // Prevent the form from trying to submit to a server
+    event.preventDefault()
+
+    // An array to be manipulated
+    let data = [120, 270, 35]
+
+    const inputField = document.querySelector('#function-2 .input')
+    const userInput = inputField.value
+    const userNumber = parseInt(userInput)
+
+    // TODO
+    const ret = data.filter( x =>  x > userNumber);
+
+    if (data.length == 0) 
+    {
+      // TODO
+      document.querySelector('#function-2 .output').innerHTML = "List had no values greater than user input";
+    }
+    else 
+    {
+      // TODO
+      document.querySelector('#function-2 .output').innerHTML = ret.toString();
+    }
 
 
-
-
+    // Clear the form field so the user can try again
+    inputField.value = ''
+  })
 })
