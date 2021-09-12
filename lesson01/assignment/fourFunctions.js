8f17569d5ee462d8f82d88cd562bf5fd71b3cb88
matthew <leematt5@uw.edu>
2021-09-12 08:28:35 -0700

    improved comments in js

--- lesson01/assignment/fourFunctions.js
+++ lesson01/assignment/fourFunctions.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function(){
   const exampleForm = document.getElementById("function-1")
 
   // Add event listner to submit event
-  exampleForm.addEventListener('onclick', function(event){
+  exampleForm.addEventListener('submit', function(event){
     
     // Prevent the form from trying to submit to a server
     event.preventDefault()
@@ -18,14 +18,14 @@ document.addEventListener("DOMContentLoaded", function(){
     const userInput = inputField.value
     const userNumber = parseInt(userInput)
 
-    // Perform a mod of each number with user input in data array and return resulting array
-    const ret = data.map( x =>  x % userNumber);
+    // Perform a mod of each number in data array with user input and return result
+    const result = data.map( x =>  x % userNumber);
 
     // Set inner html text to manipulated array
     document.querySelector('#function-1 .output').innerHTML = ret.toString();
 
     // Console log the manipualted array
-    console.log(`Template literals make things so easy! Here is the result of the prior calculation ${ret}`);
+    console.log(`Template literals make things so easy! Here is the result of the prior calculation ${result}`);
 
     // Clear the form field so the user can try again
     inputField.value = ''
@@ -52,7 +52,7 @@ document.addEventListener("DOMContentLoaded", function(){
     const userNumber = parseInt(userInput)
 
     // Remove all values in array if they are less than user input
-    const ret = data.filter( x =>  x > userNumber);
+    const result = data.filter( x =>  x > userNumber);
 
     // If array is empty set inner HTML to message, else set to array
     if (data.length == 0) 
@@ -61,7 +61,7 @@ document.addEventListener("DOMContentLoaded", function(){
     }
     else 
     {
-      document.querySelector('#function-2 .output').innerHTML = ret.toString();
+      document.querySelector('#function-2 .output').innerHTML = result.toString();
     }
 
     // Clear the form field so the user can try again
@@ -70,7 +70,7 @@ document.addEventListener("DOMContentLoaded", function(){
 })
 
 // Code that requires the DOM be loaded should not be run until the DOM is loaded
-document. addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function(){
 
   // Find the function-3 form, and handle its submit event
   const exampleForm = document.getElementById('function-3')
@@ -87,17 +87,17 @@ document. addEventListener("DOMContentLoaded", function(){
     const userNumber = parseInt(userInput)
 
     // Return array with each summed with user input
-    const ret = data.map( x =>  x + userInput*1);
+    const result = data.map( x =>  x + userInput*1);
 
     // Set inner html to array
-    document.querySelector('#function-3 .output').innerHTML = ret;
+    document.querySelector('#function-3 .output').innerHTML = result;
 
     // Clear the form field so the user can try again
     inputField.value = ''
   })
 })
 
-document. addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function(){
 
   // Find the example form, and handle its submit event
   const exampleForm = document.getElementById('function-4')
@@ -114,10 +114,10 @@ document. addEventListener("DOMContentLoaded", function(){
     const userNumber = parseInt(userInput)
 
     // Sum all values in array and multiply by user input
-    const ret = data.reduce((prev, next) => prev + next)*userNumber;
+    const result = data.reduce((prev, next) => prev + next)*userNumber;
 
     // Set inner html to array
-    document.querySelector('#function-4 .output').innerHTML = ret;
+    document.querySelector('#function-4 .output').innerHTML = result;
 
     // Clear the form field so the user can try again
     inputField.value = ''
