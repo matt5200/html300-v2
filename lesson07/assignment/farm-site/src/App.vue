be3edf68f38cf5f40e7904b1c542ac4aff0ea9de
matthew <leematt5@uw.edu>
2021-10-28 19:17:12 -0700

    init project

---
+++ lesson07/assignment/farm-site/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+  <About msg="About page"/>
+  <Testimonials msg="Testimonials page"/>
+  </div>
+</template>
+
+<script>
+import Testimonials from './components/Testimonials.vue'
+import About from './components/About.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Testimonials, 
+    About
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
