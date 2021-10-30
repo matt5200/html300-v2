34139e14105a30f8b2291b58d5a5dbd1f169f202
matthew <leematt5@uw.edu>
2021-10-29 20:39:14 -0700

    added content

--- lesson07/assignment/farm-site/src/App.vue
+++ lesson07/assignment/farm-site/src/App.vue
@@ -1,16 +1,51 @@
 <template>
 <div>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/Blog">Blog</router-link> |
-    <router-link to="/Testimonials">Testimonials</router-link> |
-    <router-link to="/About">About</router-link>
+  <div class="container">
+    <!-- Header -->
+    <div class="row header">
+        <div class="col">
+         <i class="fa fa-tractor"></i>
+        </div>
+        Blissful Farms
+        <div class="col text-end">
+            <i class="fa fa-tractor"></i>
+        </div>
+    </div>
   </div>
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="navbar">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item">
+          <router-link to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+          <router-link to="/Blog">Blog</router-link>
+          </li>
+          <li class="nav-item">
+          <router-link to="/Testimonials">Testimonials</router-link>
+          </li>
+          <li class="nav-item">
+          <router-link to="/About">About</router-link>
+          </li>
+        </ul>
+      </div>
+  </nav>
   <router-view/>
 </div>
 </template>
 
+<script>
+
+</script>
+
 <style lang="scss">
+
+$fa-font-path: "~@fortawesome/fontawesome-free/webfonts";
+@import "~@fortawesome/fontawesome-free/scss/fontawesome";
+@import "~@fortawesome/fontawesome-free/scss/solid"; // fas
+@import "~@fortawesome/fontawesome-free/scss/regular"; // far
+@import "~@fortawesome/fontawesome-free/scss/brands"; // fab
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- lesson07/assignment/farm-site/src/views/Home.vue
+++ lesson07/assignment/farm-site/src/views/Home.vue
@@ -1,6 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div class="row">
+      <!-- Create two rows in column -->
+      <div class="col mt-3">
+          <div class="row box-1 bg-light m-3">
+              <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+          </div>
+          <div class="row bg-light box-2 m-3">
+              <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+          </div>
+      </div>
+      <div class="col">
+          <!-- Create one row -->
+          <div class="row m-3">
+              <div class="row box-1 bg-light m-3">
+                  <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </p>
+                  <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </p>
+              </div>
+          </div>
+      </div>
+      <!-- Footer -->
+      <div class="row bg-light text-center">
+          © 2018 Blissful Farms, LLC | Seattle, WA
+      </div>
   </div>
 </template>
 
