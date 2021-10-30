fb895f6eaa84532d29493eb56e74e53266c88152
matthew <leematt5@uw.edu>
2021-10-30 10:51:53 -0700

    corrected blog

--- lesson07/assignment/farm-site/src/views/Blog.vue
+++ lesson07/assignment/farm-site/src/views/Blog.vue
@@ -1,32 +1,53 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="container">
+     <div class="row">
+          <!-- Image with text -->
+          <div class="col">
+              <div class="row m-3">
+              <img src="../assets/sky.jpg" class="img-fluid">
+              </div>
+          </div>
+      </div>
+      <div class="row m-3">
+          <div class="row box-1 bg-light">
+              <p class="font-italic">
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
+              </p>
+          </div>
+      </div>
+      <div class="row">
+          <!-- Image with text -->
+          <div class="col">
+              <div class="row m-3">
+              <img src="../assets/sky.jpg" class="img-fluid">
+          </div>
+      </div>
+      <div class="row m-3">
+          <div class="row box-1 bg-light">
+              <p class="font-italic">
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
+              </p>
+          </div>
+      </div>
+      <!-- Footer -->
+      <div class="row bg-light text-middle">
+          © 2018 Blissful Farms, LLC | Seattle, WA
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Blog',
-  props: {
-    msg: String
-  }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss">
+
+@import "../../scss/_base.normalize.scss";
+@import "../../scss/_components.content.scss";
+@import "../../scss/_settings.responsive.scss";
+@import "../../scss/_settings.variables.scss";
+@import "../../scss/style.scss";
+
+
 </style>
